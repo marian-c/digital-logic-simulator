ff7247d54eec83c8decfb81051ef0e24119398eb
marian-c <11059412+marian-c@users.noreply.github.com>
2025-02-20 14:49:46 +0000

    configure nextjs for trailing slashes

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
-import type { NextConfig } from "next";
+import { type NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  trailingSlash: true,
   output: 'export',
   distDir: 'out-dist'
 };
