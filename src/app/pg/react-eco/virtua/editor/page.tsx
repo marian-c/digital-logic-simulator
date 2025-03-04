bfa3fdad13e9b01e050d97d4583c3618ce31a859
marian-c <11059412+marian-c@users.noreply.github.com>
2025-03-04 16:58:49 +0000

    misc

--- src/app/pg/react-eco/virtua/editor/page.tsx
+++ src/app/pg/react-eco/virtua/editor/page.tsx
@@ -1,36 +1,60 @@
 'use client';
 import { VList } from 'virtua';
+import React from 'react';
 
-export default function PageEditorVirtuaReactEcoPg() {
+function Demo() {
   const pages = 1000;
 
-  const width = 400;
+  const width = 800;
   return (
     <div>
       <h2>Example using virtua to build the editor - simple</h2>
 
-      <VList
-        className="virtua-editor"
-        horizontal
-        style={{ height: 400, width: 400, scrollSnapType: 'x mandatory', scrollbarWidth: 'none' }}
-        onScrollEnd={() => {
-          console.log('onScrollEnd');
+      <div>
+        <VList
+          className="virtua-editor"
+          horizontal
+          style={{
+            height: 400,
+            width: width,
+            scrollSnapType: 'x mandatory',
+            scrollbarWidth: 'none',
+          }}
+          onScrollEnd={() => {
+            console.log('onScrollEnd');
+          }}
+        >
+          {Array.from({ length: pages }).map((_, i) => (
+            <div
+              key={i}
+              style={{
+                scrollSnapAlign: 'start',
+                width: width,
+                borderRight: 'solid 1px red',
+                background: 'gray',
+              }}
+            >
+              {i}
+            </div>
+          ))}
+        </VList>
+      </div>
+    </div>
+  );
+}
+
+export default function PageEditorVirtuaReactEcoPg() {
+  const [flag, setFlag] = React.useState<boolean>(false);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setFlag(!flag);
         }}
       >
-        {Array.from({ length: pages }).map((_, i) => (
-          <div
-            key={i}
-            style={{
-              scrollSnapAlign: 'start',
-              width: width,
-              borderRight: 'solid 1px red',
-              background: 'gray',
-            }}
-          >
-            {i}
-          </div>
-        ))}
-      </VList>
+        TOGGLE
+      </button>
+      {flag && <Demo />}
     </div>
   );
 }
