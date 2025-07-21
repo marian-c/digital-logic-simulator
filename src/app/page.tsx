2db09150c18adb57c2a292b6a5596f14d6996452
Rodrigo <dim3trodon@gmail.com>
2025-07-21 16:23:40 +0100

    refactor: make simulator the default page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,83 +1,328 @@
-import Image from 'next/image';
+'use client';
+
+import React from 'react';
+import {
+  defaultHeight,
+  defaultWidth,
+  gridSize,
+  notGateColor,
+  notGateHeight,
+  notGateWidth,
+  plainConnectorExtensionMin,
+} from '@/app/constants';
+import { getSample, type Sketch } from '@/app/types';
+import { assertNever } from '@/helpers/basics';
+import roundPathCorners from '@/app/rounding';
+
+type State = {
+  activeBoxId: number;
+  activeBoxPosX: number;
+  activeBoxPosY: number;
+  lastActiveBoxId: number;
+  mouseDownX: number;
+  mouseDownY: number;
+  zoomFactor: number;
+};
+
+function onElementMouseDown(
+  data: Sketch,
+  setter: React.Dispatch<React.SetStateAction<State>>,
+  elementId: number,
+) {
+  // XXX: this gets recreated on every render
+  return function (event: React.MouseEvent<SVGRectElement, MouseEvent>) {
+    if (event.button !== 0) {
+      return;
+    }
+    console.log('onElementMouseDown', event);
+    const element = data.theBox.boxElements.find((e) => e.id === elementId);
+    setter((oldState): State => {
+      return {
+        ...oldState,
+        activeBoxId: elementId,
+        activeBoxPosX: element?.pos.x || 0,
+        activeBoxPosY: element?.pos.y || 0,
+        lastActiveBoxId: elementId,
+        mouseDownX: event.clientX,
+        mouseDownY: event.clientY,
+      };
+    });
+  };
+}
+
+function onDocumentMouseUp(setter: React.Dispatch<React.SetStateAction<State>>) {
+  // XXX: this gets recreated on every render
+  return function () {
+    console.log('onDocumentMouseUp');
+    setter((oldState): State => {
+      return { ...oldState, activeBoxId: 0 };
+    });
+  };
+}
+
+// function onContainerMouseMove(
+//   state: State,
+//   setState: React.Dispatch<React.SetStateAction<State>>,
+//   setData: React.Dispatch<React.SetStateAction<Sketch>>,
+// ) {
+//   // XXX: this gets recreated on every render
+//   return function (event: React.MouseEvent<SVGSVGElement, MouseEvent>) {
+//     if (state.activeElementId) {
+//       console.log('move active element', state.activeElementId);
+//       setData((oldData): Sketch => {
+//         const elFound = oldData.theBox.elements.find((el) => el.id === state.activeElementId);
+//         if (elFound) {
+//           // XXX: this just mutates
+//           elFound.pos.x += event.clientX - state.mouseDownX;
+//           elFound.pos.y += event.clientY - state.mouseDownY;
+//           console.log('-- new pos', elFound.pos);
+//           return { ...oldData };
+//         } // XXX: else throw?
+//         return oldData;
+//       });
+//       setState({ ...state, mouseDownX: event.clientX, mouseDownY: event.clientY });
+//     }
+//   };
+// }
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{' '}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [data, setData] = React.useState<Sketch>(() => {
+    return getSample();
+  });
+  const [state, setState] = React.useState<State>({
+    activeBoxId: 0,
+    activeBoxPosX: 0,
+    activeBoxPosY: 0,
+    lastActiveBoxId: 0,
+    mouseDownX: 0,
+    mouseDownY: 0,
+    zoomFactor: 1,
+  });
+  React.useEffect(() => {
+    const cbMouseUp = onDocumentMouseUp(setState);
+    document.addEventListener('mouseup', cbMouseUp);
+    return () => {
+      document.removeEventListener('mouseup', cbMouseUp);
+    };
+  }, []);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+  const onContainerMouseMove = React.useCallback(
+    (event: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
+      if (state.activeBoxId) {
+        const elFound = data.theBox.boxElements.find((el) => el.id === state.activeBoxId);
+        const x = state.activeBoxPosX + (event.clientX - state.mouseDownX) / state.zoomFactor;
+        const y = state.activeBoxPosY + (event.clientY - state.mouseDownY) / state.zoomFactor;
+        if (elFound) {
+          // XXX: this just mutates
+          elFound.pos.x = Math.round(x / 10) * 10;
+          elFound.pos.y = Math.round(y / 10) * 10;
+          setData({ ...data });
+        } // XXX: else throw?
+
+        setState({
+          ...state,
+          mouseDownX: event.clientX,
+          mouseDownY: event.clientY,
+          activeBoxPosX: x,
+          activeBoxPosY: y,
+        });
+      }
+    },
+    [state, data],
+  );
+
+  return (
+    <div className="flex flex-row gap-x-3">
+      <svg
+        width={defaultWidth}
+        height={defaultHeight}
+        style={{
+          backgroundSize: `${gridSize * state.zoomFactor}px ${gridSize * state.zoomFactor}px`, // TODO: use vars, might be faster
+          backgroundPositionX: '-0.5px',
+          backgroundPositionY: '-0.5px',
+        }}
+        className="border border-amber-500 select-none bg-white bg-grid"
+        // onMouseMove={onContainerMouseMove(state, setState, setData)}
+        onMouseMove={onContainerMouseMove}
+      >
+        <svg
+          width={defaultWidth}
+          height={defaultHeight}
+          viewBox={`0 0 ${defaultWidth / state.zoomFactor} ${defaultHeight / state.zoomFactor}`}
         >
-          <Image aria-hidden src="/file.svg" alt="File icon" width={16} height={16} />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          {data.theBox.boxElements.map((box) => {
+            switch (box.boxKind) {
+              case 'custom':
+                // TODO
+                throw new Error('Implement this');
+                break;
+
+              case 'input':
+                // TODO
+                throw new Error('Implement this');
+                break;
+
+              case 'output':
+                // TODO
+                throw new Error('Implement this');
+                break;
+              case 'provided':
+                switch (box.providedKind) {
+                  case 'not':
+                    return (
+                      <g key={box.id} transform={`translate(${box.pos.x}, ${box.pos.y})`}>
+                        <g onMouseDown={onElementMouseDown(data, setState, box.id)}>
+                          <rect
+                            key={box.id}
+                            fill={notGateColor}
+                            width={notGateWidth}
+                            height={notGateHeight}
+                          />
+                          <text x="13" y="20" fill="white" fontWeight="bold">
+                            NOT
+                          </text>
+                        </g>
+                        <circle cx="0" cy="15" r="8" />
+                        <circle cx={notGateWidth} cy="15" r="8" />
+                      </g>
+                    );
+                  case 'and':
+                    // TODO
+                    throw new Error('Implement this');
+                  default:
+                    assertNever(box);
+                }
+                break;
+              default:
+                assertNever(box);
+            }
+          })}
+          {data.theBox.connectorElements.map((connectorElement) => {
+            switch (connectorElement.connectorKind) {
+              case 'plain': {
+                const startElement = data.theBox.boxElements.find(
+                  (e) => e.id === connectorElement.startElementId,
+                );
+                const endElement = data.theBox.boxElements.find(
+                  (e) => e.id === connectorElement.endElementId,
+                );
+                if (startElement?.elementKind !== 'box' || endElement?.elementKind !== 'box') {
+                  // TODO better handling, show an error, this just crashes
+                  throw new Error('only connecting boxes');
+                }
+
+                let actualStartPosition = { x: 0, y: 0 };
+                switch (startElement.boxKind) {
+                  case 'provided':
+                    switch (startElement.providedKind) {
+                      case 'not':
+                        actualStartPosition = {
+                          x: startElement.pos.x + notGateWidth,
+                          y: startElement.pos.y + notGateHeight / 2,
+                        };
+                        break;
+                      case 'and':
+                        // TODO:
+                        throw new Error('Implement this');
+                        break;
+                      default:
+                        assertNever(startElement);
+                    }
+                    break;
+                  case 'custom':
+                    // TODO:
+                    throw new Error('Implement this');
+                    break;
+                  default:
+                    assertNever(startElement);
+                }
+
+                let actualEndPosition = { x: 0, y: 0 };
+                switch (endElement.boxKind) {
+                  case 'provided':
+                    switch (endElement.providedKind) {
+                      case 'not':
+                        actualEndPosition = {
+                          x: endElement.pos.x,
+                          y: endElement.pos.y + notGateHeight / 2,
+                        };
+                        break;
+                      case 'and':
+                        // TODO:
+                        throw new Error('Implement this');
+                        break;
+                      default:
+                        assertNever(endElement);
+                    }
+                    break;
+                  case 'custom':
+                    // TODO:
+                    throw new Error('Implement this');
+                    break;
+                  default:
+                    assertNever(endElement);
+                }
+                console.log(
+                  'asd',
+                  roundPathCorners(
+                    `M ${actualStartPosition.x} ${actualStartPosition.y} ` +
+                      `L ${actualStartPosition.x + plainConnectorExtensionMin} ${actualStartPosition.y} ` +
+                      `L ${actualEndPosition.x - plainConnectorExtensionMin} ${actualEndPosition.y}  ` +
+                      `L ${actualEndPosition.x} ${actualEndPosition.y} `,
+                    plainConnectorExtensionMin / 2,
+                    false,
+                  ),
+                );
+                return (
+                  <path
+                    key={connectorElement.id}
+                    fill="none"
+                    stroke="black"
+                    strokeWidth={3}
+                    shapeRendering="geometricPrecision"
+                    d={roundPathCorners(
+                      `M${actualStartPosition.x} ${actualStartPosition.y} ` +
+                        `L${actualStartPosition.x + plainConnectorExtensionMin} ${actualStartPosition.y} ` +
+                        `L${actualEndPosition.x - plainConnectorExtensionMin} ${actualEndPosition.y} ` +
+                        `L${actualEndPosition.x} ${actualEndPosition.y} `,
+                      plainConnectorExtensionMin / 2,
+                      false,
+                    )}
+                  />
+                );
+              }
+              case 'smart':
+                return <></>;
+              default:
+                assertNever(connectorElement);
+            }
+          })}
+        </svg>
+        <rect fill="lightgray" width="100%" height={20} y={defaultHeight - 20} />
+      </svg>
+      <div>
+        active element id: {state.activeBoxId}
+        <br />
+        lastActiveElementId: {state.lastActiveBoxId}
+        <br />
+        Zoom factor: {state.zoomFactor}
+        <br />
+        <button
+          onClick={() => {
+            setState({ ...state, zoomFactor: state.zoomFactor + 0.1 });
+          }}
         >
-          <Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          Zoom in
+        </button>
+        <br />
+        <button
+          onClick={() => {
+            setState({ ...state, zoomFactor: state.zoomFactor - 0.1 });
+          }}
         >
-          <Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          Zoom out
+        </button>
+      </div>
     </div>
   );
 }
