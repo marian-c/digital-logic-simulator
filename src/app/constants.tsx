52bcaefd07eb9e652b5184a70713ef4866c31345
dim3 <rodriv_tf@hotmail.com>
2025-07-27 21:10:02 +0100

    feat: render input and output (and connectors) (#9)

--- src/app/constants.tsx
+++ src/app/constants.tsx
@@ -1,10 +1,27 @@
 export const defaultWidth = 800;
 export const defaultHeight = 600;
 
+export const connectorCircleRadius = 6;
+
 export const notGateWidth = 60;
 export const notGateHeight = 30;
-
 export const notGateColor = '#2c7ca3';
+
+export const inputMainCircleRadius = 10;
+export const inputLineWidth = 28;
+export const inputLineHeight = 6;
+export const inputOffMainCircleColor = '#38404c';
+export const inputLineColor = '#010101';
+export const inputSmallCircleColor = inputLineColor;
+export const inputLinePositionAdjustment = 2;
+
+export const outputMainCircleRadius = inputMainCircleRadius;
+export const outputLineWidth = inputLineWidth;
+export const outputLineHeight = inputLineHeight;
+export const outputOffMainCircleColor = inputOffMainCircleColor;
+export const outputLineColor = inputLineColor;
+export const outputSmallCircleColor = outputLineColor;
+export const outputLinePositionAdjustment = 2;
 
 export const gridSize = 40;
 
--- src/helpers/basics.ts
+++ src/helpers/basics.ts
@@ -9,12 +9,14 @@ export const canUseDOM: boolean = !!(
 export const noop = () => {};
 export const isSetTrue = (arg: boolean | undefined | null) => !!arg;
 
-export function assertNever(value: never, noThrow?: boolean): never {
+export function assertNever(value: never, noThrow?: boolean, message?: string): never {
   if (noThrow) {
     return value;
   }
 
-  throw new Error(`Unhandled discriminated union member: ${JSON.stringify(value)}`);
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}${message ? `\n${message}` : ''}`,
+  );
 }
 
 export function assertIsDefined<T>(
