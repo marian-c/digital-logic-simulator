5f46b6558c6ed8214f0aadfa5d4b769b49f27371
marian-c <11059412+marian-c@users.noreply.github.com>
2025-08-25 13:23:18 +0100

    more cleanup

--- src/app/pg/organisms/reader-abandoned/full-height/page.tsx
+++
@@ -1,6 +0,0 @@
-import { ReaderAbandoned } from '../../../../../components/_organisms/ReaderAbandoned';
-import { singuratate } from '@/data/books/samples/samples';
-
-export default function PagePgOrganismReaderFullHeight() {
-  return <ReaderAbandoned contents={singuratate} shouldUseFullHeight={true} />;
-}
--- src/app/pg/organisms/reader/module/page.tsx
+++
@@ -1,5 +0,0 @@
-import { Eminescu } from '@/components/_modules/Eminescu';
-
-export default function PagePgOrganismReaderModule() {
-  return <Eminescu />;
-}
