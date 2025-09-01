084af3b9fde8b508fbf0e566811a57e1308a6697
marian-c <11059412+marian-c@users.noreply.github.com>
2025-09-01 21:25:02 +0100

    some types and a typo

--- src/app/v2/page.tsx
+++ src/app/v2/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { Simulator } from '@/app/v2/modules/simulator';
 import { config } from '@/config';
-import { examples } from '@/app/v2/data/loadExampels';
+import { examples } from '@/app/v2/data/loadExamples';
 
 type OptionKind = 'default' | 'blank' | 'example' | 'user';
 type ValueKind = `${OptionKind}-${string}`;
---
+++ src/app/v2/types/data.ts
@@ -0,0 +1,19 @@
+// actual sketch and box connections
+export type Sketch = {
+  todo: true;
+};
+
+// positions of poxes and what not
+export type SketchPosition = {
+  todo: true;
+};
+
+// state of the inputs and other interactive elements
+export type SketchInputs = {
+  todo: true;
+};
+
+// type of the data for calculated connector states
+export type SketchState = {
+  todo: true;
+};
