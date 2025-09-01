372a64390db103d13a7b95aa3d7e873cb49514f8
Rodrigo <dim3trodon@gmail.com>
2025-09-01 12:40:58 +0100

    test some sidebar

--- src/app/v2/modules/simulator.tsx
+++ src/app/v2/modules/simulator.tsx
@@ -1,5 +1,6 @@
 import { Header } from '@/app/v2/molecules/header';
 import { CanvasV2 } from '@/app/v2/modules/canvas';
+import { Sidebar } from './sidebar';
 
 export function Simulator() {
   return (
@@ -9,6 +10,8 @@ export function Simulator() {
       </div>
       <div className="flex-grow flex">
         <CanvasV2 />
+
+        <Sidebar />
       </div>
     </div>
   );
