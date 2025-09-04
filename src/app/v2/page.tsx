44a5a514a2aaabdcdd404a825b7ac062a31d3814
Rodrigo <dim3trodon@gmail.com>
2025-09-04 14:30:12 +0100

    listen for url change wip

--- src/app/v2/page.tsx
+++ src/app/v2/page.tsx
@@ -1,13 +1,18 @@
 'use client';
+
 import { Simulator } from '@/app/v2/modules/simulator';
 import { config } from '@/config';
 import { examples } from '@/app/v2/data/loadExamples';
+import { useEffect, useState, useCallback } from 'react';
+import { useSearchParams, useRouter, usePathname } from 'next/navigation';
 
 type OptionKind = 'default' | 'blank' | 'example' | 'user';
 type ValueKind = `${OptionKind}-${string}`;
 
+const DEFAULT_OPTION = 'default-default';
+
 const options: { label: string; value: ValueKind }[] = [
-  { label: 'Load sketch..', value: 'default-default' satisfies ValueKind as ValueKind },
+  { label: 'Load sketch..', value: DEFAULT_OPTION satisfies ValueKind as ValueKind },
 ].concat(
   examples.map((e) => {
     return {
@@ -18,6 +23,31 @@ const options: { label: string; value: ValueKind }[] = [
 );
 
 export default function V2() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const pathName = usePathname();
+
+  const [selectedSketch, setSelectedSketch] = useState<ValueKind>(DEFAULT_OPTION);
+
+  // https://nextjs.org/docs/app/api-reference/functions/use-search-params#updating-searchparams
+  const updateExampleSearchParam = useCallback(
+    (value: ValueKind) => {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set('example', value);
+
+      return params.toString();
+    },
+    [searchParams],
+  );
+
+  useEffect(() => {
+    console.log('params changed', searchParams);
+    const example = searchParams.get('example') as ValueKind | null;
+    if (example) {
+      setSelectedSketch(example);
+    }
+  }, [searchParams]);
+
   return (
     <div className="flex flex-col min-h-[100vh]">
       <div className="flex flex-row justify-between">
@@ -30,9 +60,12 @@ export default function V2() {
         </div>
         <div>
           <select
-            value="default-default"
-            onChange={() => {
+            value={selectedSketch}
+            onChange={(e) => {
               console.log('change');
+
+              // setSelectedSketch(e.target.value as ValueKind);
+              router.push(`${pathName}?${updateExampleSearchParam(e.target.value as ValueKind)}`);
             }}
           >
             {options.map((option) => {
