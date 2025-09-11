eb12046b25f9e2c29fcbce85aba1c7ad20a72943
marian-c <11059412+marian-c@users.noreply.github.com>
2025-09-11 23:51:47 +0100

    example editing of meta

--- src/app/v2/modules/sidebar.tsx
+++ src/app/v2/modules/sidebar.tsx
@@ -1,10 +1,28 @@
+import { useSketchMeta } from '@/app/v2/modules/useSketchData';
+
 export function Sidebar() {
+  const { sketchMeta, setSketchMeta } = useSketchMeta();
   return (
     <div className="flex flex-col w-80 overflow-auto border border-green-500 bg-yellow-800">
       <div className="overflow-auto flex-grow h-0">
-        Sidebar
+        Sketch name: <br />
+        <input
+          disabled={sketchMeta.isExample}
+          value={sketchMeta.name}
+          onChange={(e) => {
+            setSketchMeta({ ...sketchMeta, name: e.target.value });
+          }}
+        />
         <br></br>
-        Sidebar
+        sketch description:
+        <br />
+        <input
+          disabled={sketchMeta.isExample}
+          value={sketchMeta.description}
+          onChange={(e) => {
+            setSketchMeta({ ...sketchMeta, description: e.target.value });
+          }}
+        />
         <br></br>
         Sidebar
         <br></br>
--- src/app/v2/modules/simulator.tsx
+++ src/app/v2/modules/simulator.tsx
@@ -7,6 +7,7 @@ import { useSelectedSketchInfo } from '@/app/v2/modules/useSketch';
 import { SketchDataProvider } from '@/app/v2/modules/useSketchData';
 
 const SimulatorInner: FunctionComponentWithChildren = function () {
+  console.log('R SimulatorInner');
   return (
     <SketchDataProvider>
       <div className="flex flex-grow flex-col">
@@ -24,46 +25,6 @@ const SimulatorInner: FunctionComponentWithChildren = function () {
 
 export const Simulator: FunctionComponentWithChildren = function () {
   const { selectedSketchUUID } = useSelectedSketchInfo();
-  // const selectedExample = useLocalStorageCustom(
-  //   'selectedSketch',
-  //   'default',
-  //   { kind: 'empty' },
-  //   { kind: 'example', uuid: defaultExampleUUID },
-  //   selectedSketchUUID ?? defaultExampleUUID,
-  // );
-  // let sketch: Sketch | null = null;
-  // switch (selectedExample.kind) {
-  //   case 'example':
-  //     sketch = loadExampleSketch(selectedExample.uuid);
-  //     break;
-  //   case 'user':
-  //     const userSketch = {
-  //       structure: localStorageGetItemInCollectionOrThrow(
-  //         'userSketchesStructure',
-  //         selectedExample.uuid,
-  //       ),
-  //       meta: localStorageGetItemInCollectionOrThrow('userSketchesMeta', selectedExample.uuid),
-  //       inputs: localStorageGetItemInCollectionOrThrow('userSketchesInputs', selectedExample.uuid),
-  //       positions: localStorageGetItemInCollectionOrThrow(
-  //         'userSketchesPositions',
-  //         selectedExample.uuid,
-  //       ),
-  //       simulation: localStorageGetItemInCollectionOrThrow(
-  //         'userSketchesSimulation',
-  //         selectedExample.uuid,
-  //       ),
-  //       state: localStorageGetItemInCollectionOrThrow('userSketchesState', selectedExample.uuid),
-  //     };
-  //     if (!userSketch.inputs) {
-  //       throw new Error('user sketch not found');
-  //     }
-  //     sketch = userSketch;
-  //     break;
-  //   case 'empty':
-  //     break;
-  //   default:
-  //     assertNever(selectedExample);
-  // }
 
   if (!selectedSketchUUID) {
     return <div>LOADING</div>;
