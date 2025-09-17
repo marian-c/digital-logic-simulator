0abcd1c2eec9bcea6c3c02ddfc6da43228007f9e
marian-c <11059412+marian-c@users.noreply.github.com>
2025-09-17 13:14:47 +0100

    refactor misc

--- src/app/v2/modules/interactions/provider.tsx
+++ src/app/v2/modules/interactions/provider.tsx
@@ -20,6 +20,7 @@ type Ctx = {
 const InteractionsContext = React.createContext<Ctx>(null as any);
 
 export const InteractionsProvider: FunctionComponentWithChildren = ({ children }) => {
+  // region: variables
   const mouseDocCoordinatesRefObject = React.useRef({ x: 0, y: 0 });
   const mouseCanvasCoordinatesRefObject = React.useRef<MouseCoordinates>({
     x: 0,
@@ -28,7 +29,9 @@ export const InteractionsProvider: FunctionComponentWithChildren = ({ children }
     empty: true,
   });
   const sizeRefObject = React.useRef<Size>({ width: 0, height: 0, left: 0, top: 0 });
+  // endregion
 
+  // region: helper functions
   const setCanvasCoordinates = React.useCallback(() => {
     const { x, y } = mouseDocCoordinatesRefObject.current;
     const coordX = x - sizeRefObject.current.left;
@@ -38,13 +41,16 @@ export const InteractionsProvider: FunctionComponentWithChildren = ({ children }
       coordY < 0 ||
       coordX > sizeRefObject.current.width ||
       coordY > sizeRefObject.current.height;
+
     mouseCanvasCoordinatesRefObject.current = {
       x: coordX,
       y: coordY,
       out: isOut,
     } satisfies MouseCoordinates;
   }, []);
+  // endregion
 
+  // region: event handlers
   const handleDocumentMouseMoveMouseCoordinates = React.useCallback((event: MouseEvent) => {
     const { clientX, clientY } = event;
     mouseDocCoordinatesRefObject.current = { x: clientX, y: clientY };
@@ -62,7 +68,9 @@ export const InteractionsProvider: FunctionComponentWithChildren = ({ children }
     };
     setCanvasCoordinates();
   }, []);
+  // endregion
 
+  // region: react interface
   const svgRef = React.useCallback((el: SVGSVGElement | null) => {
     // attach zoom handler, would eventually need panning coordinates, which would be good to be refs
     // since we only need it in callbacks - no use rendering this context when that changes
@@ -83,8 +91,8 @@ export const InteractionsProvider: FunctionComponentWithChildren = ({ children }
       );
     }
   }, []);
-
   const canvasRef = useElementLayoutWithRef<HTMLDivElement>(handleLayoutEvent);
+  // endregion
 
   const contextVal = React.useMemo(() => {
     return { svgRef, canvasRef };
