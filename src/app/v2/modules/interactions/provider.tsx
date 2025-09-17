794a880f50f5361ab510f0079b818ee66aecba85
marian-c <11059412+marian-c@users.noreply.github.com>
2025-09-17 11:37:37 +0100

    refactor: tidy up

--- src/app/v2/modules/interactions/provider.tsx
+++ src/app/v2/modules/interactions/provider.tsx
@@ -1,6 +1,7 @@
 import type { FunctionComponentWithChildren } from '@/types/r-ui';
 import React from 'react';
 import { useElementLayoutWithRef } from '@/hooks/useElementLayout/useElementLayoutWithRef';
+import type { LayoutEvent } from '@/types/rnw';
 
 type Size = { width: number; height: number; left: number; top: number };
 
@@ -19,37 +20,63 @@ type Ctx = {
 const InteractionsContext = React.createContext<Ctx>(null as any);
 
 export const InteractionsProvider: FunctionComponentWithChildren = ({ children }) => {
-  const mouseCoordinatesRef = React.useRef<MouseCoordinates>({
+  const mouseDocCoordinatesRefObject = React.useRef({ x: 0, y: 0 });
+  const mouseCanvasCoordinatesRefObject = React.useRef<MouseCoordinates>({
     x: 0,
     y: 0,
     out: false,
     empty: true,
   });
-  const sizeRef = React.useRef<Size>({ width: 0, height: 0, left: 0, top: 0 });
+  const sizeRefObject = React.useRef<Size>({ width: 0, height: 0, left: 0, top: 0 });
+
+  const setCanvasCoordinates = React.useCallback(() => {
+    const { x, y } = mouseDocCoordinatesRefObject.current;
+    const coordX = x - sizeRefObject.current.left;
+    const coordY = y - sizeRefObject.current.top;
+    const isOut =
+      coordX < 0 ||
+      coordY < 0 ||
+      coordX > sizeRefObject.current.width ||
+      coordY > sizeRefObject.current.height;
+    mouseCanvasCoordinatesRefObject.current = {
+      x: coordX,
+      y: coordY,
+      out: isOut,
+    } satisfies MouseCoordinates;
+  }, []);
+
+  const handleDocumentMouseMoveMouseCoordinates = React.useCallback((event: MouseEvent) => {
+    const { clientX, clientY } = event;
+    mouseDocCoordinatesRefObject.current = { x: clientX, y: clientY };
+    setCanvasCoordinates();
+  }, []);
+
+  const handleSvgWheelPinch = React.useCallback((wheelEvent: WheelEvent) => {}, []);
+
+  const handleLayoutEvent = React.useCallback((layoutEvent: LayoutEvent) => {
+    sizeRefObject.current = {
+      width: layoutEvent.nativeEvent.layout.width,
+      height: layoutEvent.nativeEvent.layout.height,
+      left: layoutEvent.nativeEvent.layout.left,
+      top: layoutEvent.nativeEvent.layout.top,
+    };
+    setCanvasCoordinates();
+  }, []);
+
   const svgRef = React.useCallback((el: SVGSVGElement | null) => {
     // attach zoom handler, would eventually need panning coordinates, which would be good to be refs
     // since we only need it in callbacks - no use rendering this context when that changes
     if (el) {
-      document.addEventListener('mousemove', (event) => {
-        const coordX = event.clientX - sizeRef.current.left;
-        const coordY = event.clientY - sizeRef.current.top;
-        const isOut =
-          coordX < 0 ||
-          coordY < 0 ||
-          coordX > sizeRef.current.width ||
-          coordY > sizeRef.current.height;
-        mouseCoordinatesRef.current = {
-          x: coordX,
-          y: coordY,
-          out: isOut,
-        } satisfies MouseCoordinates;
-      });
+      document.addEventListener('mousemove', handleDocumentMouseMoveMouseCoordinates);
       el.addEventListener(
         'wheel',
         (wheelEvent) => {
+          wheelEvent.preventDefault();
           if (wheelEvent.ctrlKey) {
             // pinch
-            wheelEvent.preventDefault();
+            handleSvgWheelPinch(wheelEvent);
+          } else {
+            // pan
           }
         },
         { passive: false },
@@ -57,15 +84,7 @@ export const InteractionsProvider: FunctionComponentWithChildren = ({ children }
     }
   }, []);
 
-  const canvasRef = useElementLayoutWithRef<HTMLDivElement>((e) => {
-    console.log('setting the size', e.nativeEvent.layout);
-    sizeRef.current = {
-      width: e.nativeEvent.layout.width,
-      height: e.nativeEvent.layout.height,
-      left: e.nativeEvent.layout.left,
-      top: e.nativeEvent.layout.top,
-    };
-  });
+  const canvasRef = useElementLayoutWithRef<HTMLDivElement>(handleLayoutEvent);
 
   const contextVal = React.useMemo(() => {
     return { svgRef, canvasRef };
