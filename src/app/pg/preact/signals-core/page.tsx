1d5c936380293ba9fd444bd206e8ce6458f44e53
marian-c <11059412+marian-c@users.noreply.github.com>
2025-09-21 21:37:30 +0100

    add preact signals

---
+++ src/app/pg/preact/signals-core/page.tsx
@@ -0,0 +1,56 @@
+import { computed, effect, signal } from '@/imported/preact-signals-core';
+console.log('start');
+const counter = signal(0);
+
+console.log('signal value with peek', counter.peek());
+
+const counterSubscribed = computed(() => {
+  console.log('Calculate counter subscribed...');
+  return counter.value + 1;
+});
+
+const counterPeek = computed(() => {
+  console.log('Calculate counter peek...');
+  return counter.peek() + 1;
+});
+
+/**
+ * updating initial signals relation to computed signals
+ */
+console.log('Initial, updating...');
+counter.value = counter.value + 1;
+
+console.log('getting the value of the computed signals', {
+  sub: counterSubscribed.value,
+  peek: counterPeek.value,
+});
+
+console.log('again...');
+// only the subscribed signals updates, the peek one is left alone
+counter.value = counter.value + 1;
+
+console.log('getting the value of the computed signals', {
+  sub: counterSubscribed.value,
+  peek: counterPeek.value,
+});
+
+/**
+ * effect
+ */
+const name = signal('Jane');
+const surname = signal('Doe');
+const fullName = computed(() => name.value + ' ' + surname.value);
+
+// Logs: "Jane Doe", logs twice
+effect(() => console.log('twice', fullName.value));
+
+// this effect logs only once
+effect(() => console.log('once', fullName.peek()));
+
+// Updating one of its dependencies will automatically trigger
+// the effect above, and will print "John Doe" to the console.
+name.value = 'John'; // logs again
+
+export default function PagePgPreactSignalsCore() {
+  return <div>See the console and code</div>;
+}
