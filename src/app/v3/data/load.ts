dad89aafc80f6473fea16123d1bfa21d0de3bbbb
marian-c <11059412+marian-c@users.noreply.github.com>
2025-10-14 11:37:16 +0300

    add NAND box sample and include it in examples list

--- src/app/v3/data/load.ts
+++ src/app/v3/data/load.ts
@@ -1,5 +1,6 @@
 import { sketch as sketch1 } from '@/app/v3/data/samples/x-1';
 import { sketch as sketch2 } from '@/app/v3/data/samples/stress_100_close';
 import { sketch as sketch3 } from '@/app/v3/data/samples/stress_500_close';
+import { sketch as boxNand } from '@/app/v3/data/samples/box-nand';
 
-export const examplesV3 = [sketch1, sketch2, sketch3];
+export const examplesV3 = [sketch1, sketch2, sketch3, boxNand];
