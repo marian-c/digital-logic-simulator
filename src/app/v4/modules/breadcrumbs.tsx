fb1f112139f58a6ca1313e5f69b50e6e60c7e09d
marian-c <11059412+marian-c@users.noreply.github.com>
2026-01-22 11:14:38 +0200

    update

--- src/app/v4/modules/breadcrumbs.tsx
+++ src/app/v4/modules/breadcrumbs.tsx
@@ -2,6 +2,6 @@ import type { FunctionComponentWithChildren } from '@/types/r-ui';
 
 export const Breadcrumbs: FunctionComponentWithChildren = function () {
   return (
-    <div className="px-2 py-0 border-y border-gray-300 bg-gray-100 text-base">🎛️ {'>'} stuff</div>
+    <div className="px-2 py-0 border-y border-gray-300 bg-gray-100 text-base">🎛️ {'>'} ...</div>
   );
 };
